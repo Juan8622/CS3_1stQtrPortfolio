9c9e651cac752158ba2a369ef57dc8d45d198751
Juan8622 <b2029jmfmendoza@pshs.edu.ph>
2025-08-05 11:50:09 +0800

    Second Commit

---
+++ public/script.js
@@ -0,0 +1,7 @@
+var age = 14;
+var maxAge = 100;
+var numPerday = 100
+
+var totalRequired = (numPerDay * 365) * (maxAge - age);
+
+document.write('You will need ${totalRequired} raymond ashly c calitis to last you untill he ripe old age of ${maxAge})
