ca3aec706a07b7a0cca9826f0912a41df4982aad
Juan8622 <b2029jmfmendoza@pshs.edu.ph>
2025-08-12 11:38:24 +0800

    st#1

--- public/script.js
+++ public/script.js
@@ -4,4 +4,4 @@ var numPerday = 100
 
 var totalRequired = (numPerDay * 365) * (maxAge - age);
 
-document.write('You will need ${totalRequired} raymond ashly c calitis to last you untill he ripe old age of ${maxAge})
+document.write('You will need ' + totalRequired + ' raymond ashly c calitis to last you untill he ripe old age of ' + maxAge + ', you stupid nigger')
